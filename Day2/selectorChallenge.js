28fa1803c5b1da15bf2908ffe19fc7b6497b6d6f
joshwoodrum <joshuawoodrum@JoshuaWoodrumMBP.local>
2016-09-20 09:41:20 -0400

    stuff

---
+++ Day2/selectorChallenge.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$("button").click(function(){
+$("#element").hide();
+});
+});
+
+$(document).ready(function(){
+$("button").click(function(){
+$("#h3").hide();
+});
+});
+
---
+++ Oldscripts/DAY ONE/jquery3.js
@@ -0,0 +1,15 @@
+function myButton_Click() {
+    alert("Clicked!");
+}
+
+
+$(function() {
+    // Wire up the click event handler.
+    $("#myButton").on("click", myButton_Click);
+    $("li:contains('mad')").on("click", function(){
+        $(this).hide(); 
+
+});
+});
+
+
